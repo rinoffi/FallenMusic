8411d336fb5bc37456826c158207730491a2fa4c
rinoffi <sivaneshoffi@gmail.com>
2025-05-30 17:14:21 +0530

    Update config.py
    Signed-off-by: rinoffi <sivaneshoffi@gmail.com>

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("28015531"))
+API_HASH = getenv("2ab4ba37fd5d9ebf1353328fc915ad28")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+BOT_TOKEN = getenv("7277505434:AAErB4dH09Qq7EZWTp_anDdYsX4FkQUqhH8", None)
+DURATION_LIMIT = int(getenv("90", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID"))
+OWNER_ID = int(getenv("7874828505"))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
