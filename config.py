ee3f1d4a7b3c778c868053ce0dfe82dddc361f0c
rinoffi <sivaneshoffi@gmail.com>
2025-05-30 17:26:48 +0530

    Update config.py
    Signed-off-by: rinoffi <sivaneshoffi@gmail.com>

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ DURATION_LIMIT = int(getenv("90", "90"))
 
 OWNER_ID = int(getenv("7874828505"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
+START_IMG = getenv("https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("https://t.me/+b7ZuDQ3sPdE3ZTll")
+SUPPORT_CHANNEL = getenv("https://t.me/KHOLI_THIRUDAN_GROUP")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
